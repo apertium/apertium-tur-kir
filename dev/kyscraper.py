c3d8e356dc2ed333d9de5af4835ff05aa3204d33
Jonathan North Washington <jonathan.n.washington@gmail.com>
2011-06-26 07:45:07 +0000

    python3 port

--- dev/kyscraper.py
+++ dev/kyscraper.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python
-# coding=utf-8
-# -*- encoding: utf-8 -*-
 
-import sys, codecs, copy, os;
+import sys, codecs, copy, os, re;
 import feedparser;
-import pycurl;
-
-sys.stdin  = codecs.getreader('utf-8')(sys.stdin);
-sys.stdout = codecs.getwriter('utf-8')(sys.stdout);
-sys.stderr = codecs.getwriter('utf-8')(sys.stderr);
+import urllib;
 
 cur_title = '';
 contents = '';
+sterilisation = [('&quot;', '"'),
+	('&ldquo;', '“'),
+	('&rdquo;', '”'),
+	('&laquo;', '«'),
+	('&raquo;', '»'),
+	('&ndash;', '–'),
+	('&mdash;', '—'),
+	('&nbsp;', ' ')]
+
+pageParser = {}
+pageParser["trt"] = [
+	('<br /></strong>', '</strong><br />'),
+	('<br />', '\n<br />\n'),
+	('</p>', '\n</p>\n'),
+	('</div>', '\n</div>\n'),
+	('<p class="introduction">', '\n<p class="introduction">\n'),
+	('<div class="author">', '\n<div class="author">\n'),
+	('<div class="zoomMe">', '\n<div class="zoomMe">\n'),
+	('<div class="date">', '\n<div class="date">\n')]
 
 def read_page(buf): #{
 	global contents;
@@ -20,30 +33,37 @@ def read_page(buf): #{
 #}
 
 def parse_html(s): #{
-	s = s.replace('&quot;', '"');
-	s = s.replace('&ldquo;', '“');
-	s = s.replace('&rdquo;', '”');
-	s = s.replace('&laquo;', '«');
-	s = s.replace('&raquo;', '»');
-	s = s.replace('&ndash;', '–');
-	s = s.replace('&mdash;', '—');
-	s = s.replace('&nbsp;', ' ');
+	global sterilisation
+	for (subj, repl) in sterilisation:
+		s = re.sub(subj, repl, s)
+	#s = s.replace('&quot;', '"');
+	#s = s.replace('&ldquo;', '“');
+	#s = s.replace('&rdquo;', '”');
+	#s = s.replace('&laquo;', '«');
+	#s = s.replace('&raquo;', '»');
+	#s = s.replace('&ndash;', '–');
+	#s = s.replace('&mdash;', '—');
+	#s = s.replace('&nbsp;', ' ');
 	
 	return s;
 #}
 
 def parse_page(buf): #{
+	global pageParser
 	out = '';
 	printing = False;
 
-	buf = buf.replace('<br /></strong>', '</strong><br />');
-	buf = buf.replace('<br />', '\n<br />\n');
-	buf = buf.replace('</p>', '\n</p>\n');
-	buf = buf.replace('</div>', '\n</div>\n');
-	buf = buf.replace('<p class="introduction">', '\n<p class="introduction">\n');
-	buf = buf.replace('<div class="author">', '\n<div class="author">\n');
-	buf = buf.replace('<div class="zoomMe">', '\n<div class="zoomMe">\n');
-	buf = buf.replace('<div class="date">', '\n<div class="date">\n');
+	for (subj, repl) in pageParser["trt"]:
+		buf = re.sub(subj, repl, buf)
+		print(buf)
+	#buf = buf.replace('<br /></strong>', '</strong><br />');
+	#buf = buf.replace('<br />', '\n<br />\n');
+	#buf = buf.replace('</p>', '\n</p>\n');
+	#buf = buf.replace('</div>', '\n</div>\n');
+	#buf = buf.replace('<p class="introduction">', '\n<p class="introduction">\n');
+	#buf = buf.replace('<div class="author">', '\n<div class="author">\n');
+	#buf = buf.replace('<div class="zoomMe">', '\n<div class="zoomMe">\n');
+	#buf = buf.replace('<div class="date">', '\n<div class="date">\n');
 
 	for line in buf.split('\n'): #{
 		if line.count('class="detay_aciklama">') > 0: #{
@@ -61,25 +81,25 @@ def parse_page(buf): #{
 	return out;
 #}
 
-
 urls = [];
 
-for line in file('kyscraper_rss.dat').read().split('\n'): #{
+for line in open('kyscraper_rss.dat').read().split('\n'): #{
 	urls.append(line);
 #}
 	
 for url in urls: #{
-	print '+' , url;
+	print ('+' , url);
 	feed = feedparser.parse(url);
 	
 	for item in feed["entries"]: #{
 		contents = '';
 		aid = '';
 		for link in item["links"]:
-			print '++' , link["href"] ;
+			print('++' , link["href"]);
 			aid = link["href"].split('=')[1];
-			print aid;
-			filename = str('./articles/' + aid + '.html');
+			print(aid);
+			#filename = str('./articles/' + aid + '.html');
+			filename = "./articles/%s.html" % aid
 	
 			if aid == '' and contents == '': #{
 				continue;
@@ -90,26 +110,23 @@ for url in urls: #{
 			#}
 		
 			if os.path.isfile(filename): #{
-				print filename , ' file exists.';
+				print(filename , ' file exists.');
 				continue;
 			#}
 	
 			cur_title = item["title"];
-			print '++' , cur_title;
+			print('++' , cur_title);
 	
-			print '++' , item["link"];
+			print('++' , item["link"]);
+
+			f = urllib.request.urlopen(item["link"])
+			contents = f.read()
 	
-			c = pycurl.Curl();
-			c.setopt(c.URL, str(item["link"]));
-			c.setopt(c.WRITEFUNCTION, read_page);
-			c.perform();
-			c.close();
+			content = parse_page(contents.decode('utf-8'));
 	
-			content = parse_page(contents);
-	
-			f = file(filename, 'w+');
+			f = open(filename, 'w+');
 			f.write('\n<div class="title">\n');
-			f.write('<h1>' + cur_title + '</h2>\n');
+			f.write('<h1>' + cur_title + '</h1>\n');
 			f.write('\n</div>\n');
 			f.write(content);
 			f.close();
