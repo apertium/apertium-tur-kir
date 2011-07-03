296f768ff49c0fa94b0202a404334012e9cc2711
Jonathan North Washington <jonathan.n.washington@gmail.com>
2011-07-03 09:00:21 +0000

    Some updated random stuff

--- dev/scraper.py
+++ dev/scraper.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 from scraperclasses import Feed
+import argparse, sys
+
+parser = argparse.ArgumentParser(description='Scrape RSS feeds to build corpora.')
+parser.add_argument('feedlist', nargs='?', type=argparse.FileType('r'),
+	default=sys.stdin, help='a file containing a list of rss feeds, separated by newlines')
+parser.add_argument('-o', '--outdir', dest='outdir', default='corpus.ky',
+	help='the directory to output the corpus to')
+
+args = parser.parse_args()
+
 
 urls = [];
 
-for line in open('kyscraper_rss.dat').read().split('\n'): #{
-	urls.append(line);
-#}
+if args.feedlist != None:
+	#print(args.feedlist[0])
+	for line in args.feedlist: #{
+		urls.append(line);
+	#}
+
 	
-for url in urls: #{
-	print ('+' , url);
-	feed = Feed(url)
+	for url in urls: #{
+		print ('+' , url);
+		feed = Feed(url)
+		for source in feed.get_sources():
+			print(source)
+			source.add_to_archive(args.outdir)
+
+
+else:
+	parser.print_help()
--- dev/scraperclasses.py
+++ dev/scraperclasses.py
@@ -1,16 +1,87 @@
 #!/usr/bin/env python3
 
 import re, feedparser, urllib, os
+from bottle import template
+from hashlib import sha1
 
 class Scraper(object):
 
 	content = None
+	domain = None
 
 	def __init__(self, content):
 		self.content = content
 
+class ScraperKMB(Scraper):
+	replacements = []
+	sterilisation = []
+	domain = "kmb3.kloop.kg"
+	prefix = "kmb3"
+
+	def url_to_aid(url):
+		return sha1(url).hexdigest()
+		#return url.split('=')[1]
+	
+	def parse_html(self, s):
+		for (patt, repl) in self.sterilisation:
+			s = re.sub(patt, repl, s)
+	
+		return s;
+
+
+	def scrape(self, content=None):
+		if not content and self.content:
+			content = self.content
+
+		out = '';
+		printing = False;
+
+		for (subj, repl) in self.replacements:
+			content = re.sub(subj, repl, content)
+
+		for line in content.split('\n'):
+			out = out + self.parse_html(line) + '\n';
+
+		return out;
+
+
+class ScraperAzattyk(Scraper):
+	replacements = []
+	sterilisation = []
+	domain = "www.azattyk.org"
+	prefix = "rflre"
+
+	def url_to_aid(url):
+		return sha1(url).hexdigest()
+		#return url.split('=')[1]
+	
+	def parse_html(self, s):
+		for (patt, repl) in self.sterilisation:
+			s = re.sub(patt, repl, s)
+	
+		return s;
+
+
+	def scrape(self, content=None):
+		if not content and self.content:
+			content = self.content
+
+		out = '';
+		printing = False;
+
+		for (subj, repl) in self.replacements:
+			content = re.sub(subj, repl, content)
+
+		for line in content.split('\n'):
+			out = out + self.parse_html(line) + '\n';
+
+		return out;
+
 
 class ScraperTRT(Scraper):
+
+	domain = "www.trtkyrgyz.com"
+	prefix = "trt"
 
 	replacements = [
 		('<br /></strong>', '</strong><br />'),
@@ -20,7 +91,11 @@ class ScraperTRT(Scraper):
 		('<p class="introduction">', '\n<p class="introduction">\n'),
 		('<div class="author">', '\n<div class="author">\n'),
 		('<div class="zoomMe">', '\n<div class="zoomMe">\n'),
-		('<div class="date">', '\n<div class="date">\n')
+		('<div class="date">', '\n<div class="date">\n'),
+		#Below here are JNW's additions:
+		('<p>', ''),
+		('<div.*?>', ''),
+		('</div>', '')
 	]
 
 	sterilisation = [
@@ -50,7 +125,6 @@ class ScraperTRT(Scraper):
 		if not content and self.content:
 			content = self.content
 
-		#global pageParser
 		out = '';
 		printing = False;
 
@@ -77,10 +151,13 @@ class ScraperTRT(Scraper):
 
 
 class Feed(object):
+
 	domain_name = re.compile('^http[s]{0,1}://(.*?)/.*')
 
 	feed_sites = {
-		"www.trtkyrgyz.com": ScraperTRT
+		"www.trtkyrgyz.com": ScraperTRT,
+		"www.azattyk.org": ScraperAzattyk,
+		"kmb3.kloop.kg": ScraperKMB
 	}
 
 	which_scraper = None;
@@ -88,14 +165,16 @@ class Feed(object):
 
 	def __init__(self, url):
 		self.which_scraper = self.get_scraper(url)
-		self.feed = feedparser.parse(url);
+		self.feed = feedparser.parse(url)
+		print(self.feed)
 		
+	def get_sources(self):
 		for item in self.feed["entries"]:
 			for link in item["links"]:
 				title = item["title"]
 				print('++' , title);
 				print('++' , item["link"]);
-				Source(item["link"], scraper=self.which_scraper, title=title)
+				yield Source(item["link"], scraper=self.which_scraper, title=title)
 	
 	def get_scraper(self, url):
 		domain = self.domain_name.match(url).group(1)
@@ -120,7 +199,9 @@ class Source(object):
 	out_content = None
 
 	def __init__(self, url, scraper=None, title=None):
-		self.set_url(url)
+		#self.set_url(url)
+		self.url = url
+		self.title = title
 		if not scraper:
 			self.scraper = self.get_scraper(url)
 		else: self.scraper = scraper
@@ -128,14 +209,13 @@ class Source(object):
 			raise Exception("No scraper set!")
 
 		self.aid = self.scraper.url_to_aid(url)
-		self.filename = "./articles/%s.html" % self.aid
+		self.filename = self.scraper.prefix+".%s.html" % self.aid
 
 		print(self.aid)
 
 		if not self.filename_exists():
 			self.page_contents = self.get_page(url)
 			self.out_content = self.get_content()
-			self.add_to_archive()
 		else:
 			print(self.filename, " exists!")
 
@@ -153,16 +233,19 @@ class Source(object):
 		scraper = self.scraper(contents)
 		return scraper.scrape()
 
-	def set_url(self, url):
-		self.url = url
+	#def set_url(self, url):
+	#	self.url = url
 	
 	def filename_exists(self):
 		return os.path.isfile(self.filename)
 	
-	def add_to_archive(self):
-		f = open(self.filename, 'w+');
-		f.write('\n<div class="title">\n');
-		f.write('<h1>' + str(self.title) + '</h1>\n');
-		f.write('\n</div>\n');
-		f.write(self.out_content);
-		f.close();
+	def add_to_archive(self, outdir):
+		#print(outdir, self.filename, self)
+		with open(os.path.join(outdir, self.filename), 'w+') as f:
+		#f = open(os.path.join(outdir, self.filename), 'w+');
+			f.write(template('source', title=self.title, content=self.out_content))
+			#f.write('\n<div class="title">\n');
+			#f.write('<h1>' + str(self.title) + '</h1>\n');
+			#f.write('\n</div>\n');
+			#f.write(self.out_content);
+		#f.close();
