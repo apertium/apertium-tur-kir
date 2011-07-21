6768774ada030643eb4728e5fd223dc6909f0a9f
Jonathan North Washington <jonathan.n.washington@gmail.com>
2011-07-21 10:36:31 +0000

    moving

--- dev/corpora/scraper.py
+++ dev/corpora/scraper.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-from scraperclasses import Feed, SimpleHtmlScraper
+from scraperclasses import Feed
 import argparse, sys
-
 
 parser = argparse.ArgumentParser(description='Scrape RSS feeds to build corpora.')
 parser.add_argument('feedlist', nargs='?', type=argparse.FileType('r'),
 	default=sys.stdin, help='a file containing a list of rss feeds, separated by newlines')
 parser.add_argument('-o', '--outdir', dest='outdir', default='corpus.ky',
 	help='the directory to output the corpus to')
-parser.add_argument('-r', '--rss', dest='read_as_rss', default=True, help='whether or not to assume source is rss')
 
 args = parser.parse_args()
+
 
 urls = [];
 
@@ -24,15 +23,10 @@ if args.feedlist != None:
 	
 	for url in urls: #{
 		print ('+' , url);
-		if bool(int(args.read_as_rss)):
-			feed = Feed(url)
-			for source in feed.get_sources():
-				print(source)
-				source.add_to_archive(args.outdir)
-		else:
-			page = SimpleHtmlScraper("history", url, args.outdir)
-			page.parse()
-			page.write()
+		feed = Feed(url)
+		for source in feed.get_sources():
+			print(source)
+			source.add_to_archive(args.outdir)
 
 
 else:
