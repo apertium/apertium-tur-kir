d68727b47ea86ada30b00e0b8d7ea5835def0f05
Brendan Molloy <brendan@bbqsrc.net>
2011-07-31 10:58:08 +0000

    workaround for shit non-conforming nothing syntax with <tags>"

--- dev/corpora/scraped2html.py
+++ dev/corpora/scraped2html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import xml.etree.ElementTree as etree
+#import xml.etree.ElementTree as etree
 import os
 import sys
 
@@ -14,11 +14,24 @@ files = os.listdir('.')
 
 for fn in files:
 	print(fn)
-	root = etree.parse(fn).getroot()
-	for item in root.getiterator("div"):
-		if item.attrib['class'] == 'content':
-			output.write(item.text.strip() + '\n')
-			break
+	f = open(fn, 'r')
+	x = False
+	for line in f:
+		if line.strip() == '<div class="content">':
+			x = False
+		elif x == True:
+			if line.strip() == "</div>":
+				break
+			elif line.strip() != "":
+				output.write(line.strip() + "\n")	
+	f.close()
+			
+
+	#root = etree.parse(fn).getroot()
+	#for item in root.getiterator("div"):
+	#	if item.attrib['class'] == 'content':
+	#		output.write(item.text.strip() + '\n')
+	#		break
 
 output.close()
 print("Done.")
